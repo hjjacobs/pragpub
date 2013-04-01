61d5cbe4aba29772854806cf25f522fc699d289a
HJ Jacobs <hj.jacobs@gmail.com>
2013-04-01 14:57:03 +0200

    First commit

---
+++ pragpub.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+#
+# Script for downloading the current PragPub (http://pragprog.com/magazines) in mobi format
+# and mailing the file to my kindle.
+#
+
+# Location settings
+DOWNLOAD_PATH="$( cd "$( dirname "$0" )" && pwd )"
+
+# WGet options: normal verbose (nv) directory prefix (-P)
+WGET_OPTS="-nv -P ${DOWNLOAD_PATH}"
+
+# Pragmatic Pub settings
+PB_YEAR=`date +%Y`
+PB_MONTH=`date +%m`
+PB_MAGAZINE="pragpub-${PB_YEAR}-${PB_MONTH}.mobi"
+PB_URL="http://magazines.pragprog.com/${PB_YEAR}/${PB_MAGAZINE}"
+
+# Mail settings
+KINDLE_ADDRESS="PUT YOUR KINDLE MAIL ADDRESS HERE"
+
+function mail-mobi {
+	echo "Mailing ${PB_MAGAZINE} to ${KINDLE_ADDRESS}" 1>&2
+
+	echo | mutt -s "Sending ${PB_MAGAZINE} to kindle" -a ${DOWNLOAD_PATH}/${PB_MAGAZINE} -- ${KINDLE_ADDRESS} 1>&2
+}
+
+echo "Checking if ${DOWNLOAD_PATH}/${PB_MAGAZINE} is present"  1>&2
+
+
+if [[ ! -f "${DOWNLOAD_PATH}/${PB_MAGAZINE}" ]]; then
+	echo "Downloading ${PB_MAGAZINE}"  1>&2
+
+	wget ${WGET_OPTS} ${PB_URL}
+	
+	if [[ $? -eq 0 ]]; then
+		mail-mobi
+	else
+		echo "Unable to download ${PB_MAGAZINE}. May not yet be available."  1>&2
+	fi
+else
+	echo "${PB_MAGAZINE} already downloaded." 1>&2
+	
+	if [[ $1 = "-m" ]]; then
+		mail-mobi
+	else
+		echo "Doing nothing.." 1>&2
+	fi
+fi
+
---
+++ readme.md
@@ -0,0 +1,45 @@
+Pragpub
+=======
+
+Bash script for downloading the current (as in the month this script is started)
+PragPub ([http://pragprog.com/magazines][1]) in mobi format and mailing the file
+to your kindle.
+
+Use your favorite scheduler (e.g. cron) in order to automate sending your
+monthly PragPub to your kindle .
+
+
+
+Prerequisites
+-------------
+
+-   The [mutt][2] E-mail client
+
+[2]: <http://www.mutt.org>
+
+-   The [wget][3] tool
+
+[3]: <http://www.gnu.org/software/wget/>
+
+-   Your system must be able to send mail to, in this case, Amazon
+
+
+
+Settings
+--------
+
+Set the KINDLE_ADDRESS to an email address which is allowed to the receive your
+content.
+
+
+
+Command line options
+--------------------
+
+The script works without any parameters.
+
+-   m  - if you want to (re-)send the current pragpub magazine
+
+
+
+[1]: <http://pragprog.com/magazines>
